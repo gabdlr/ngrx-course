dc5c558623de779c2385653f2a4ca8025406e612
Gabriel De Los Rios <gab.delosrios@gmail.com>
2023-01-19 23:55:38 -0300

    refactor: switches to onPush chng detctn strategy

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,4 +1,9 @@
-import { AfterViewInit, Component, OnInit } from "@angular/core";
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+} from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Course } from "../model/course";
 import { Observable, of } from "rxjs";
@@ -21,6 +26,7 @@ import { LessonsEntityService } from "../services/lessons-entity.service";
   selector: "course",
   templateUrl: "./course.component.html",
   styleUrls: ["./course.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CourseComponent implements OnInit {
   course$: Observable<Course>;
